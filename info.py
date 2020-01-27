89128b9138675f29dccbe176485cc84c27662e9c
BSmegha <bsmegha81@gmail.com>
2020-01-27 12:57:15 +0530

    changed

--- info.py
+++ info.py
@@ -2,3 +2,4 @@ import pandas as pd
 data={"name":["athula","arya","yedu","jose"],"college":["CEA","CET","TKM","SNIT"],"admn.no":["A123","A456","A789","A086"],"roll.no":["001","002","003","004"]}
 df=pd.DataFrame(data)
 print(df)
+print(df)
